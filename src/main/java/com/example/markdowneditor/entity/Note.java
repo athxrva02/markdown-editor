f787f84c451ee0fbec27e978522a8d22abdb826e
atharva <dagaonkaratharva@gmail.com>
2025-07-01 00:08:37 +0530

    Add H2 config and Note Entity

--- src/main/java/com/example/markdowneditor/entity/Note.java
+++ src/main/java/com/example/markdowneditor/entity/Note.java
@@ -1,4 +1,30 @@
 package com.example.markdowneditor.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Entity(name = "Note")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "updated_date")
+    private Date updatedDate;
 }
