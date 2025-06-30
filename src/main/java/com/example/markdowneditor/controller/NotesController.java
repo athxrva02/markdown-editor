97fbff79db9e0f4a15450875cb110958c6d9e8aa
atharva <dagaonkaratharva@gmail.com>
2025-06-30 23:54:02 +0530

    Complete createNote controller

--- src/main/java/com/example/markdowneditor/controller/NotesController.java
+++ src/main/java/com/example/markdowneditor/controller/NotesController.java
@@ -2,11 +2,14 @@ package com.example.markdowneditor.controller;
 
 import com.example.markdowneditor.dto.request.CreateNoteReqDTO;
 import com.example.markdowneditor.dto.response.CommonResponse;
+import com.example.markdowneditor.enums.ResponseStatus;
 import com.example.markdowneditor.service.NotesService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.ThreadContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController("notes")
 public class NotesController {
 
@@ -14,6 +17,14 @@ public class NotesController {
 
     public CommonResponse createNote(@RequestBody CreateNoteReqDTO createNoteReqDTO) {
         ThreadContext.put("API", "createNote");
-        Note
+        try {
+            notesService.createNote(createNoteReqDTO);
+            return new CommonResponse(ResponseStatus.SUCCESS, null, "Note created successfully");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new CommonResponse(ResponseStatus.ERROR, null, e.getMessage());
+        } finally {
+            ThreadContext.remove("API");
+        }
     }
 }
